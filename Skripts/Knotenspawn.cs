84504f8f8ffa17868c6b07b64a8e602533ad5509
BFT6D4E <luk2912@gmail.com>
2018-01-26 23:43:12 +0100

    KnotenSpawn ( random)

---
+++ Skripts/Knotenspawn.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class Knotenspawn : MonoBehaviour
+{
+
+    public float centerX = 0f;
+    public float centerY = 0f;
+    public float increasment = 1.1f;
+    public string nodeTag;
+
+
+
+    public GameObject triangle;
+    public GameObject circle;
+    public GameObject square;
+
+    public float minDistance;
+
+    public float spawnTime;
+
+    private static float maxDistance = 1;
+
+    private GameObject[] nodes;
+
+
+
+
+    private void Start()
+    {
+        InvokeRepeating("Spawn", spawnTime, spawnTime);
+    }
+
+    private void Spawn()
+    {
+        if (maxDistance == 0)
+            maxDistance = 1;
+
+        bool succes = false;
+        float ranX = 0;
+        float ranY = 0;
+        while (!succes)
+        {
+            ranX = Random.Range(0.1f, maxDistance);
+            ranY = Random.Range(0.1f, (float)(System.Math.Sqrt((maxDistance * maxDistance + ranX * ranX))));
+
+            nodes = GameObject.FindGameObjectsWithTag(nodeTag);
+
+            int counter = 0;
+
+            foreach (GameObject go in nodes)
+            {
+                if (GiveDistance(go, ranX, ranY) < minDistance)
+                {
+                    if (counter == 3)
+                        maxDistance = maxDistance * increasment;
+
+                    counter++;
+                    break;
+                }
+
+                succes = true;
+            }
+        }
+
+        Vector2 pos = new Vector2(ranX, ranY);
+
+        int number = (int)Random.Range(0, 3);
+        if (number == 3)
+            number = 2;
+
+        switch (number)
+        {
+            case 0:
+                Instantiate(triangle, pos, new Quaternion(0, 0, 0, 0));
+                break;
+            case 1:
+                Instantiate(circle, pos, new Quaternion(0, 0, 0, 0));
+                break;
+            case 2:
+                Instantiate(square, pos, new Quaternion(0, 0, 0, 0));
+                break;
+
+        }
+
+    }
+
+    private float GiveDistance(GameObject g, float x, float y)
+    {
+        Vector2 a = gameObject.transform.transform.position;
+
+        bool equal = a.x == x && a.y == y;
+
+        if (equal)
+            return 0;
+
+        float distance = (float)System.Math.Sqrt((a.x - x) * (a.x - x) + (a.y - y) * (a.y - y));
+
+
+        return distance;
+    }
+}
