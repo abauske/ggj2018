ac67db6cf2b3a4eaf4f8f6a9c187ea8945713038
Adrian Bauske <adrian.bauske@gmail.com>
2018-01-27 22:15:10 +0100

    selectable synapses

---
+++ Scripts/Synapses/CircleSynapse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleSynapse : Synapse {
+
+    private float transferspeed = 8;
+
+    public Shape transferable = Shape.CIRCLE;
+
+    public override bool canTransfer(Shape data)
+    {
+        float duration = Weight / transferspeed;
+
+        if (data == transferable && Time.time > lastTransmissionStart + duration)
+        {
+            return true;
+        }
+        Debug.Log("We have to wait: " + (Time.time - lastTransmissionStart - duration));
+        return false;
+    }
+}
---
+++ Scripts/UI/SynapseSelector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SynapseSelector : MonoBehaviour
+    , IPointerClickHandler // 2
+    , IDragHandler
+    , IPointerEnterHandler
+    , IPointerExitHandler
+// ... And many more available!
+{
+
+    void Update()
+    {
+        if (!EventSystem.current.IsPointerOverGameObject())
+        {
+            print("I was over");
+        }
+    }
+
+    public void OnPointerClick(PointerEventData eventData) // 3
+    {
+        print("I was clicked");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        print("I'm being dragged!");
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("I'm being dragged!");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("I'm being dragged!");
+    }
+}
