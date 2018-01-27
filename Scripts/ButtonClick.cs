f21fc048a7824eabb41eaf90efb7ba05f49594a0
Adrian Bauske <adrian.bauske@gmail.com>
2018-01-27 23:20:48 +0100

    colorcoding active synapse

---
+++ Scripts/ButtonClick.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonClick : MonoBehaviour
+{
+    private static SynapseButton selected = SynapseButton.DEFAULT;
+    public SynapseButton button;
+
+    void FixedUpdate()
+    {
+        if (button == selected)
+        {
+            gameObject.GetComponent<Button>().Select();
+        }
+    }
+
+    public void setDefaultSyn()
+    {
+        selected = SynapseButton.DEFAULT;
+    }
+    public void setFastSyn()
+    {
+        selected = SynapseButton.FAST;
+    }
+    public void setTriangleSyn()
+    {
+        selected = SynapseButton.TRIANGLE;
+    }
+    public void setCircleSyn()
+    {
+        selected = SynapseButton.CIRCLE;
+    }
+    public void setSquareSyn()
+    {
+        selected = SynapseButton.SQUARE;
+    }
+}
+
+public enum SynapseButton
+{
+    DEFAULT,
+    FAST,
+    SQUARE,
+    TRIANGLE,
+    CIRCLE
+}
