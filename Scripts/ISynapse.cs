13bb1af99e8e8615c05d3ecb8b2904559429fd0e
haertlsimon <haertlsimon@gmail.com>
2018-01-27 20:53:37 +0100

    removed button color changer script

---
+++ Scripts/ISynapse.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ISynapse : MonoBehaviour {
+
+    // shapes that can be transferred
+    List<Shape> shapes;
+
+
+
+}
