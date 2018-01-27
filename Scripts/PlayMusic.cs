2b7e5882e1ebab6cd5aac0cfcc4803af6cfe0f24
snowballbird <snowballbird53@gmail.com>
2018-01-27 16:55:21 +0100

    added highscore and sound script

---
+++ Scripts/PlayMusic.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMusic : MonoBehaviour {
+    AudioSource m_MyAudioSource;
+    GameObject myNode;
+	// Use this for initialization
+	void Start () {
+        m_MyAudioSource = GetComponent<AudioSource>();
+        myNode = this.gameObject;
+    }
+    private void Update()
+    {
+        float posx = myNode.transform.position.x;
+        Debug.Log(posx);
+        if (Input.GetKeyDown("return"))
+        {
+            PlaySound();
+        }
+    }
+    
+    void PlaySound() {
+        float posx=myNode.transform.position.x;
+        m_MyAudioSource.pitch = 1+posx*0.05f;
+        m_MyAudioSource.Play();
+        
+	}
+}
