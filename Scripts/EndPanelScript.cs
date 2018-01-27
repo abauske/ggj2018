b5b1fc8b9d2310f4e6986c4a2980b03f481e162c
haertlsimon <haertlsimon@gmail.com>
2018-01-27 17:47:18 +0100

    weird amount of data

--- Scripts/EndPanelScript.cs
+++ Scripts/EndPanelScript.cs
@@ -1,8 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndPanelScript : MonoBehaviour {
+    
+    public int score;
 
-	
+    private Text endText;
+    private GameObject endPanel;
+    private GameObject menuPanel;
+
+    public void endGame()
+    {
+        endText = GetComponent<Text>();
+        endPanel = GetComponent<GameObject>();
+        menuPanel = GameObject.Find("barPanel");
+        endPanel.SetActive(true);
+        menuPanel.SetActive(false);
+        endText.text = string.Format("You lost!\nYour score:\n{0:00000}", score);
+    }
 }
