6014b01f965bb8f4127173e8a052ee7fb6cd11e0
TrafalgarDLaw2018 <warum98@gmx.net>
2018-01-27 15:50:23 +0100

    Data Math

--- Scripts/NodeDataSpawner.cs
+++ Scripts/NodeDataSpawner.cs
@@ -9,8 +9,8 @@ public class NodeDataSpawner : Node
     private List<Data_Script> daten = new List<Data_Script>();
     private int lostDataCount = 6;
     public int spawnSpeed = 10;
-
-    private int Vectorlength = 1;
+    private int datenAmount = 0;
+    private double Vectorlength = 0.5;
     private int counter = 0;
     private float spawnTime;
     public float spawnIntervall = 6;
@@ -18,7 +18,7 @@ public class NodeDataSpawner : Node
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (daten.Count > lostDataCount)
+        if (datenAmount >= lostDataCount)
         {
             //Game end.
         }
@@ -28,36 +28,28 @@ public class NodeDataSpawner : Node
             {
                 spawnTime = Time.time + spawnIntervall;
                 counter += 1;
+                datenAmount += 1;
                 Shape s;
                 do
                 {
                     s = (Shape) Random.Range(0, System.Enum.GetNames(typeof(Shape)).Length);
                 } while (s == shape);
-                 
 
-                float x = this.transform.position.x;
-                float y = this.transform.position.y;
+                GameObject dataObject = Instantiate(dataPrefab, this.transform.position, new Quaternion(0, 0, 0, 0), this.transform);
 
-                float winkel = 360 / lostDataCount;
-                winkel *= daten.Count;
-
-                y += Vectorlength * (float) System.Math.Sin(winkel);
-                x += Vectorlength * (float)System.Math.Cos(winkel);
-
-
-                GameObject dataObject = Instantiate(dataPrefab, new Vector2(x,y), new Quaternion(0, 0, 0, 0), this.transform);
+                Vector3 v = arangeData();
+                dataObject.transform.position += v;
 
                 dataObject.SetActive(true);
                 var d = dataObject.GetComponent<Data_Script>();
-                // d.transform.SetParent(this.transform);
                 d.setShape(s);
 //                dataObject.transform.SetParent(this.transform);
 //                dataObject.transform.position = transform.position + Vector3.down;
 //                dataObject.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-                addData(d);
                 
-
-
+                //daten.Add(d);
+                
+                
                 if ((counter % spawnSpeed) == 0) // alle 10 counts wird schneller gespawnt
                 {
                     spawnIntervall = spawnIntervall * Random.Range(0.98f, 0.99f);
@@ -68,11 +60,48 @@ public class NodeDataSpawner : Node
         this.trySendData();
     }
 
-    public void addData(Data_Script data)
+    
+    public Vector3 arangeData()
     {
-        daten.Add(data);
-        gameObject.transform.position = this.transform.position;
+        float x = 0;
+        float y = 0;
+
+        float winkel = (float) (2 * System.Math.PI) / lostDataCount;
+        Debug.Log(winkel + ", " + winkel * (datenAmount));
+        winkel = winkel * (float)(datenAmount) ;
+        
+
+        x = (float)(Vectorlength * (System.Math.Cos(winkel)));
+        y = (float)(Vectorlength * (System.Math.Sin(winkel)));
+        Debug.Log(x + ", " + y);
+        return new Vector3(x,y,0);
     }
+    
+    /*
+        public Vector3 arangeData(int i)
+    {
+        return new Vector3(i,1.0f,0);
+    }
+    */
+
+    public void rearangeData()
+    {
+        int i = 0;
+        foreach (Data_Script d in daten)
+        {
+            d.transform.position += arangeData();
+            i++;
+        }
+
+    }
+
+    public void addData(Data_Script newData)
+    {
+        daten.Add(newData);
+        rearangeData();
+    }
+
+    
 
     private void trySendData()
     {
