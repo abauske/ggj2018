a626dfe347a5a91f33aedf4c498f9800c20314d5
haertlsimon <haertlsimon@gmail.com>
2018-01-27 10:03:15 +0100

    Added Menu Bar v1

---
+++ Scripts/UIBarButtons.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBarButtons : MonoBehaviour {
+
+    private Button thisButton;
+    private float height;
+    private float width;
+    private Canvas canvas;
+    	
+	void Start ()
+    {        
+        height = 30;
+        
+        thisButton = GetComponent<Button>();
+        setSize();
+
+        canvas = GetComponent<Canvas>();
+	}
+
+    public void Update()
+    {
+        setSize();
+    }
+
+    private void setSize()
+    {
+        width = Screen.width/8;
+        thisButton.image.rectTransform.sizeDelta = new Vector2(width, height);
+    }
+}
