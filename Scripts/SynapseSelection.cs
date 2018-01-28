d5436ed51e9087c05ddeec27d876b4cf8663376a
BFT6D4E <luk2912@gmail.com>
2018-01-28 16:21:15 +0100

    triangle selector deactivated

--- Scripts/SynapseSelection.cs
+++ Scripts/SynapseSelection.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SynapseSelection : MonoBehaviour {
 
     public Type syn = typeof(DefaultSynapse);
-    public int price = 20;
+    public int price = 25;
 
     public void setDefaultSyn()
     {
