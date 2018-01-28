e4f22a8f3c43e1842029c0f35aa48ac822ec04cb
snowballbird <snowballbird53@gmail.com>
2018-01-28 10:23:46 +0100

    Playmusic fix

--- Scripts/PlayMusic.cs
+++ Scripts/PlayMusic.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayMusic : MonoBehaviour {
     AudioSource m_MyAudioSource;
     GameObject myNode;
-	// Use this for initialization
-	void Start () {
+    Shape data;
+    public AudioClip fortriangle;
+    public AudioClip forcircle;
+    public AudioClip forsquare;
+    // Use this for initialization
+    void Start () {
         m_MyAudioSource = GetComponent<AudioSource>();
         myNode = this.gameObject;
     }
@@ -19,11 +23,18 @@ public class PlayMusic : MonoBehaviour {
             PlaySound();
         }
     }
-    
-    void PlaySound() {
-        float posx=myNode.transform.position.x;
-        m_MyAudioSource.pitch = 1+posx*0.05f;
+
+    public void PlaySound() {
+        switch (data) {
+            case Shape.TRIANGLE: m_MyAudioSource.clip = fortriangle; break;
+            case Shape.CIRCLE: m_MyAudioSource.clip = forcircle; break;
+            case Shape.SQUARE: m_MyAudioSource.clip = forsquare; break;
+            default: break;
+        }
+       
+         float posx = myNode.transform.position.x;
+        m_MyAudioSource.pitch = 1 + posx * 0.05f;
         m_MyAudioSource.Play();
-        
+      
 	}
 }
