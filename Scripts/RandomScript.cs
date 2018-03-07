d3f2a798bd04af6cf5ce8440185174d2715a40f9
BFT6D4E <luk2912@gmail.com>
2018-03-07 23:12:29 +0100

    edited UI,
    ToggleMenu for GameVersion

---
+++ Scripts/RandomScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class RandomScript : MonoBehaviour {
+
+    private bool selected;
+
+	// Use this for initialization
+	void Start () {
+		selected = GameObject.FindGameObjectWithTag("GameVersion_Random").GetComponent<Toggle>().isOn;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        bool temp = GameObject.FindGameObjectWithTag("GameVersion_Random").GetComponent<Toggle>().isOn;
+        if(temp != selected)
+        {
+            selected = temp;
+            if (temp)
+            {
+                GameObject.FindGameObjectWithTag("GameVersion_sorted").GetComponent<Toggle>().isOn = false;
+                var e = GameObject.FindGameObjectWithTag("Container").GetComponent<Containmentscript>();
+                if (e != null)
+                {
+                    e.gameVersion = 2;
+
+                }
+            }
+            else
+            {
+                GameObject.FindGameObjectWithTag("GameVersion_sorted").GetComponent<Toggle>().isOn = true;
+                var e = GameObject.FindGameObjectWithTag("Container").GetComponent<Containmentscript>();
+                if (e != null)
+                {
+                    e.gameVersion = 1;
+
+                }
+            }
+        }
+    }
+}
