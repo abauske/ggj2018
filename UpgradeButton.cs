41d233b2da16d76e124e5aaeae3f8e56fa30e2da
TrafalgarDLaw2018 <warum98@gmx.net>
2018-03-09 17:57:42 +0100

    *

---
+++ UpgradeButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeButton : MonoBehaviour {
+    private int[] Lv;
+    private int thisLv;
+    private Text[] c;
+    private string explainText;
+
+    public void Start()
+    {
+        Lv = GameObject.FindGameObjectWithTag("Container").GetComponent<Containmentscript>().Lv;
+        thisLv = Lv[0];
+    }
+
+    public void onClick()
+    {
+        c = this.GetComponentsInChildren<Text>();
+        explainText = "Cost: " + c[0].text + "\n";
+        explainText += "LV: " + thisLv + c[1].text + "\n";
+        explainText += c[2].text;
+        GameObject.FindGameObjectWithTag("ExplainText").GetComponent<Text>().text = explainText;
+    }
+
+}
