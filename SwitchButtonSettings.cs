587ea6bef844f9c2d1fb75b186ba1e5b34e6a550
TrafalgarDLaw2018 <warum98@gmx.net>
2018-03-09 14:25:48 +0100

    Update Scene aktualisiert

---
+++ SwitchButtonSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SwitchButtonSettings : MonoBehaviour {
+
+	public void switchPannels(bool b)
+    {
+        if(b)
+        {
+            GameObject.FindGameObjectWithTag("UpgradesPanel").GetComponent<RectTransform>().localPosition = new Vector3( -1650 , 0 , 0);
+            GameObject.FindGameObjectWithTag("GameSettingPanel").GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+            GameObject.FindGameObjectWithTag("ExplainText").GetComponent<Text>().text = "";
+        }
+        else
+        {
+            GameObject.FindGameObjectWithTag("UpgradesPanel").GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+            GameObject.FindGameObjectWithTag("GameSettingPanel").GetComponent<RectTransform>().localPosition = new Vector3(1650, 0, 0);
+
+        }
+    }
+}
